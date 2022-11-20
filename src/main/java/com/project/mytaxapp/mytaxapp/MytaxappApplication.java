114bc7ccddab02d88b2921974fa42b998f32480c
DiegoAmaxmandro <102663353+DiegoAmaxmandro@users.noreply.github.com>
2022-11-20 19:38:22 +0000

    Initial Commit

---
+++ src/main/java/com/project/mytaxapp/mytaxapp/MytaxappApplication.java
@@ -0,0 +1,13 @@
+package com.project.mytaxapp.mytaxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytaxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytaxappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mytaxapp/mytaxapp/MytaxappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mytaxapp.mytaxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytaxappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
